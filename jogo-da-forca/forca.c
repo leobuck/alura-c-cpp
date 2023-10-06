7250deb2d07a6ede4f228f7d937f53e72277d005
leobuck <leobuck12@gmail.com>
2023-10-05 21:48:21 -0300

    Percorrendo o array

--- jogo-da-forca/forca.c
+++ jogo-da-forca/forca.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char palavrasecreta[20];
@@ -9,6 +10,14 @@ int main() {
     int enforcou = 0;
 
     do {
+        char chute;
+        scanf("%c", &chute);
+
+        for (int i = 0; i < strlen(palavrasecreta); i++) {
+            if (palavrasecreta[i] == chute) {
+                printf("A posicao %d tem essa letra!\n", i);
+            }
+        }
 
     } while(!acertou && !enforcou);
 }
